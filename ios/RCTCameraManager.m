89e2d8707fe392f92ad01194be885b202299f084
Radu Popovici <radu.popovici@munichgeneral.com>
2016-03-24 10:12:57 +0200

    refactor sensor orientation checker

--- ios/RCTCameraManager.m
+++ ios/RCTCameraManager.m
@@ -248,6 +248,7 @@ RCT_CUSTOM_VIEW_PROPERTY(barCodeTypes, NSArray, RCTCamera) {
     self.mirrorImage = false;
 
     self.sessionQueue = dispatch_queue_create("cameraManagerQueue", DISPATCH_QUEUE_SERIAL);
+    self.sensorOrientationChecker = [RCTSensorOrientationChecker new];
   }
   return self;
 }
@@ -495,7 +496,7 @@ RCT_EXPORT_METHOD(hasFlash:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRej
       NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
       [self saveImage:imageData target:target metadata:nil resolve:resolve reject:reject];
 #else
-      [self.sensorOrientationChecker getDeviceOrientation:^(UIInterfaceOrientation orientation) {
+      [self.sensorOrientationChecker getDeviceOrientationWithBlock:^(UIInterfaceOrientation orientation) {
           [[self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo] setVideoOrientation:[self.sensorOrientationChecker convertToAVCaptureVideoOrientation: orientation]];
 
       [self.stillImageOutput captureStillImageAsynchronouslyFromConnection:[self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo] completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
